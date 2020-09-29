bb45d769f82c4e3dbd736db26faba14bf2c8a378
AlanMartines <alancpmartines@hotmail.com>
2020-09-29 17:19:55 -0400

    Update

--- routes/sistem.js
+++ routes/sistem.js
@@ -125,7 +125,6 @@ router.post("/sendText", async (req, res, next) => {
 }); //sendText
 //
 router.post("/sendTextMult", upload.single('sendTextMassaContato'), async (req, res, next) => {
-
     var result = await Sessions.sendTextMult(
         req.body.SessionName,
         req.file.buffer.toString('base64'),
@@ -133,36 +132,6 @@ router.post("/sendTextMult", upload.single('sendTextMassaContato'), async (req, 
         req.file.originalname,
         req.body.msgtxtmass
     );
-    /*
-    var folderName = fs.mkdtempSync(path.join(os.tmpdir(), req.body.SessionName + '-'));
-    var filePath = path.join(folderName, req.file.originalname);
-    fs.writeFileSync(filePath, req.file.buffer.toString('base64'), 'base64');
-    console.log(filePath);
-    //
-    var jsonStr = '{"sendResult":[]}';
-    var obj = JSON.parse(jsonStr);
-    /*
-    obj['sendTextMult'].push({
-        "teamId": "4",
-        "status": "pending"
-    });
-    
-    //
-    var arrayNumbers = fs.readFileSync(filePath, 'utf-8').toString().split(/\r?\n/);
-    for (i in arrayNumbers) {
-        console.log(arrayNumbers[i]);
-        var result = await Sessions.sendTextMult(
-            req.body.SessionName,
-            arrayNumbers[i],
-            req.body.msgtxtmass
-        );
-        obj['sendResult'].push(result);
-    }
-    //
-    jsonStr = JSON.stringify(obj);
-    console.log(JSON.parse(jsonStr));
-    //return JSON.parse(jsonStr);
-    */
     res.json(result);
 }); //sendText
 //
