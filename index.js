7c79adcffe4e2339060e94ff5d58e91dc37a62a1
Alan Cleber Pereira Martines <alancpmartines@hotmail.com>
2020-10-04 19:14:39 -0400

    Update

--- index.js
+++ index.js
@@ -21,6 +21,13 @@ web.use(cors());
 web.use(express.json());
 //
 // Configuração
+/*
+web.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+*/
 // Body Parser
 web.use(bodyParser.json());
 web.use(bodyParser.urlencoded({
